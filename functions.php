2b01ba96c6d5964b6fac86684004744e3411a60a
gassrobertpaul <gassrobertpaul@yahoo.com>
2017-12-19 14:34:17 -0800

    add some styling for search box

--- functions.php
+++ functions.php
@@ -1,19 +1,11 @@
 <?php 
 
-/*function admin_bar(){
-	show_admin_bar( true );
-   if(is_user_logged_in()){
-    add_filter( 'show_admin_bar', '__return_true' , 1000 );
-  } 
-}
-add_action('init', 'admin_bar' );
-function my_function_admin_bar(){
-	return true;
-}
-add_filter( 'show_admin_bar' , 'my_function_admin_bar')
-*/
-
 require get_template_directory() . '/inc/cleanup.php';
 require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/function-admin.php';
 
+// Register Menu	
+	function register_my_menu() {
+	  register_nav_menu('header-menu',__( 'Header Menu' ));
+	}
+	add_action( 'init', 'register_my_menu' );
