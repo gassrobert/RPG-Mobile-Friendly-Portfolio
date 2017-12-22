7da882359ecbbefab1802d7c22c01a401af57432
gassrobertpaul <gassrobertpaul@yahoo.com>
2017-12-21 19:32:37 -0800

    Style the search form

--- functions.php
+++ functions.php
@@ -4,8 +4,8 @@ require get_template_directory() . '/inc/cleanup.php';
 require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/function-admin.php';
 
-// Register Menu	
-	function register_my_menu() {
-	  register_nav_menu('header-menu',__( 'Header Menu' ));
-	}
-	add_action( 'init', 'register_my_menu' );
+/* Activate Nav Menu Option */
+function rpg_portfolio_register_nav_menu() {
+	register_nav_menu( 'primary', 'Header Navigation Menu' );
+}
+add_action( 'after_setup_theme', 'rpg_portfolio_register_nav_menu' );
