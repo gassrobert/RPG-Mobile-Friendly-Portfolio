6bfd1d610d8fd9c6387a45ee969f931f19eb262f
gassrobertpaul <gassrobertpaul@yahoo.com>
2017-12-22 14:46:15 -0800

    add main menu

--- functions.php
+++ functions.php
@@ -6,6 +6,6 @@ require get_template_directory() . '/inc/function-admin.php';
 
 /* Activate Nav Menu Option */
 function rpg_portfolio_register_nav_menu() {
-	register_nav_menu( 'primary', 'Header Navigation Menu' );
+	register_nav_menu( 'rpg-profile-primary-menu', 'Header Navigation Menu' );
 }
-add_action( 'after_setup_theme', 'rpg_portfolio_register_nav_menu' );
+add_action( 'init', 'rpg_portfolio_register_nav_menu' );
--- header.php
+++ header.php
@@ -1,5 +1,17 @@
   <div id="header">
     <!-- <h1>Header</h1> -->
+	<div class="nav-collapse collapse navbar-responsive-collapse">
+		<nav>
+			<div class="leftMainMenu">  
+				<?php 
+					wp_nav_menu(array(
+						'theme_location' => 'rpg-profile-primary-menu', 
+						'container' => false
+					)); 
+				?>
+			</div>
+		</nav>
+	</div> 
     <?php 
     get_search_form();
     ?>
