005b20fbbcd4a2cccc0b4b0b7f8c530c79ccdecc
gassrobertpaul <gassrobertpaul@yahoo.com>
2017-12-21 19:46:27 -0800

    make footer dynamic

--- footer.php
+++ footer.php
@@ -1,3 +1,11 @@
+<?php
+if(!get_option('profile_name')){
+    // update_option('profile_name', 'first_default_value');
+	$profilename = "Default User Name";
+} else {
+	$profilename = esc_attr( get_option( 'profile_name' ) );	
+}
+?>
   <div id="footer">
-    <p>Copyright &copy; 2017-<?php echo date("Y");?> <br />Robert Paul Gass Profile <br /> All rights reserved </p>
+    <p>Copyright &copy; <?php echo date("Y");?> <br /><?php print $profilename; ?> Profile <br /> All rights reserved </p>
   <!-- end #footer --></div>
