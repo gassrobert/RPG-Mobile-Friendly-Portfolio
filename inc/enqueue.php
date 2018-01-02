3f399c59bb124d3c60568582ab3b25b222029dca
gassrobertpaul <gassrobertpaul@yahoo.com>
2018-01-02 11:40:05 -0800

    add styling to footer widgets

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,7 +11,7 @@
 
 function rpg_portfolio_load_scripts(){
 	
-	wp_enqueue_style( 'rpg_front_style', get_template_directory_uri() . '/css/rpg_front_style.css', array(), '1.0.2', 'all' );
+	wp_enqueue_style( 'rpg_front_style', get_template_directory_uri() . '/css/rpg_front_style.css', array(), '1.0.3', 'all' );
 
 }
 add_action( 'wp_enqueue_scripts', 'rpg_portfolio_load_scripts' );
