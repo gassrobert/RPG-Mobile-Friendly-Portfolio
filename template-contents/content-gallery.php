cdebf981990bd02f84490fcba351f83a3ff849b8
gassrobertpaul <gassrobertpaul@yahoo.com>
2018-01-25 20:06:38 -0800

    add styling to gallery navigation buttons

--- template-contents/content-gallery.php
+++ template-contents/content-gallery.php
@@ -39,14 +39,23 @@
           <?php endforeach; ?>
 
         <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"><</span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true">></span>
-          <span class="sr-only">Next</span>
-        </a>
+        <div class="preview-container">
+          <span class="thumbnail-container background-image"></span>
+          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"><</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </div>
+
+        <div class="preview-container">
+          <span class="thumbnail-container background-image"></span>
+          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true">></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+
+
         </div>  <!-- end of carousel-inner -->
       </div> <!-- end of carousel slide -->
     <?php // endif; ?>
