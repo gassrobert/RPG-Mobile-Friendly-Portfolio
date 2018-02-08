c4171c0d86f1a562e8d96d8769c8d60fe25c394e
gassrobertpaul <gassrobertpaul@yahoo.com>
2018-02-07 19:46:53 -0800

    Switch fadeToggle with toggleClass for the sidebar in mobile devices

--- js/rpgprofile.front.js
+++ js/rpgprofile.front.js
@@ -11,9 +11,7 @@ jQuery(document).ready(function($) {
 
 	// Mobile sidebar toggling
 	$(document).on('click', '.js-toggleSidebar', function() {
-		$( '#sidebar1' ).fadeToggle( 320 );
-		$( '#sidebar1' ).css("width", "100%");
-		$( '#sidebar1' ).css("z-index", "9999");		
+		$( '#sidebar1' ).toggleClass( 'sidebarToggle');	
 		$( '.sidebar-overlay' ).fadeToggle( 320 );
 		$( 'body' ).toggleClass( 'no-scroll' );
 	});	
