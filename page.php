1fa5300bf9365bb04693be9df3843a3b59d69a3c
gassrobertpaul <gassrobertpaul@yahoo.com>
2018-02-14 13:50:23 -0800

    fix title function parameters

--- page.php
+++ page.php
@@ -17,7 +17,7 @@
 		<div id="articleContainer">
 
 						<header class="entry-header">
-						<?php the_title( '<h1 class="entry-title"></h1>' ); ?>
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 						</header>
 
